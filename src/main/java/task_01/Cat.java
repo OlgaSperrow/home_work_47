299b0634764feeed15f3de72b963b04287438059
OlgaSperrow <osperling17@gmail.com>
2025-01-29 15:01:32 +0100

    Task 01 added

---
+++ src/main/java/task_01/Cat.java
@@ -0,0 +1,14 @@
+package task_01;
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
